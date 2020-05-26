dba997ff63c27c73a39e535a074820528b5d19f5
Sarah Hart Landolt <sarah.hart.landolt@gmail.com>
2020-05-26 16:05:34 -0500

    completed fizzbuzz

---
+++ fizzbuzz/csharp/fizzbuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n <= 100; n++)
+            {
+                if (n % 15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (n % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (n % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(n);
+                }
+
+            }
+
+        }
+    }
+}
