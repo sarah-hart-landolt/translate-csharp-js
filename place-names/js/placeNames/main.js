dd33457d918f59066d6568634a3c10b07a34c7d8
Sarah Hart Landolt <sarah.hart.landolt@gmail.com>
2020-05-26 13:54:34 -0500

    ex 1

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,5 +1,25 @@
 function main() {
   // Put your code here
+  const names = [ "Nashville", "Hong Kong", "The back yard", "Earth",
+  "London", "The mall", "Ryman Auditorium", "The Great Wall of China"];
+
+  console.log("All Place Names"); 
+
+  for (let name of names) {
+    console.log(name);
+  };
+
+console.log();
+
+theNames = names.filter((n)=>n.startsWith("The"));
+
+console.log("'The' Place Names");
+for  (name of theNames) 
+{
+  console.log(name);
+}
+
+
 
 }
 
